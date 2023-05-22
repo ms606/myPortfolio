a78220e70b05dab540730c364cfc3cd927d08bed
ms606 <muffi606@gmail.com>
2023-05-22 23:37:20 +0500

    js addition files

---
+++ script.js
@@ -0,0 +1,6 @@
+let toggler = document.getElementsByClassName('toggler')
+let shortButton = document.getElementById('short')
+
+
+console.log(toggler,'toggler')
+console.log(shortButton,'shortButton')
