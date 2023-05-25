829286e45a7fb521e912199c681cf8998c46cdc7
ms606 <muffi606@gmail.com>
2023-05-25 23:25:32 +0500

    js implementation

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-let toggler = document.getElementsByClassName('toggler')
-let shortButton = document.getElementById('short')
+// let toggler = document.getElementsByClassName('toggler')
+//let shortButton = document.getElementById('short')
+
+// (function () {
+// 	setBioEventListener();
+// })();
+
+setBioEventListener();
+
+/* Bio toggles */
+function setBioEventListener() {
+	Array.from(document.getElementsByTagName("button")).forEach((e) => {
+     e.addEventListener("click", bioToggle);
+	});
+}
+
+function bioToggle(e){
+    let element = document.getElementsByClassName("bio short");
+    console.log(element);
+    element.classList.add('show')
+}
+
+setBioEventListener();
 
 
-console.log(toggler,'toggler')
-console.log(shortButton,'shortButton')
